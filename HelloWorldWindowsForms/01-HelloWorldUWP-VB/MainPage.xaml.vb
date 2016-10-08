2eb827de300237a853e67b5627f3a74f95f2064b
Francisco Javier Ruiz Rodriguez <Pajarrurro@gmail.com>
2016-10-08 12:50:12 +0200

    Todo en sus correspondientes carpetas(más o menos)

---
+++ HelloWorldWindowsForms/01-HelloWorldUWP-VB/MainPage.xaml.vb
@@ -0,0 +1,21 @@
+﻿' La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+''' <summary>
+''' Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Page
+
+    Private Sub boton_Click(sender As Object, e As RoutedEventArgs) Handles boton.Click
+        Dim nomb As String = nombre.Text
+        Dim fecha As String
+        If (Date.Now.Hour >= 6 And Date.Now.Hour < 12) Then
+            fecha = "buenos días"
+        ElseIf (Date.Now.Hour >= 12 And Date.Now.Hour < 22) Then
+            fecha = "buenas tardes"
+        Else
+            fecha = "buenas noches"
+        End If
+        muestranombre.Text = "Hola " + nomb + "," + fecha
+    End Sub
+End Class
---
+++ HelloWorldWindowsForms/01-HelloWorldWPF-VB/MainWindow.xaml.vb
@@ -0,0 +1,33 @@
+﻿Class MainWindow
+    ''' <summary>
+    ''' funcionalidad que al clickar el botón nos muestra un hola mundo con el nombre
+    ''' de la persona y Buenos días, Buenas tardes o Buenas noches dependiendo de la hora
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <pre>Ninguna</pre>
+    ''' <post>Mostrará en un cuadro de texto el hola más le nombre más los buenos ....</post>
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Dim nombre As String = txtNombre.Text
+        Dim fecha As String
+        Dim bi3 As New BitmapImage
+        If (Date.Now.Hour >= 6 And Date.Now.Hour < 12) Then
+            fecha = "buenos días"
+            bi3.BeginInit()
+            bi3.UriSource = New Uri("C:\Users\fjrodriguez\Downloads\mañana.jpg")
+            bi3.EndInit()
+        ElseIf (Date.Now.Hour >= 12 And Date.Now.Hour < 22) Then
+            fecha = "buenas tardes"
+            bi3.BeginInit()
+            bi3.UriSource = New Uri("C:\Users\fjrodriguez\Downloads\tarde.jpg")
+            bi3.EndInit()
+        Else
+            fecha = "buenas noches"
+            bi3.BeginInit()
+            bi3.UriSource = New Uri("C:\Users\fjrodriguez\Downloads\noche.jpg")
+            bi3.EndInit()
+        End If
+        imagen.Source = bi3
+        muestraNombre.Text = "Hola " + nombre + "," + fecha
+    End Sub
+End Class
